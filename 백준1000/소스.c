9e7e89ca1336fcf3d9d7d76c2a6363b6b1a2c9a3
혜민 최 <pavi03@kpu.ac.kr>
2020-07-23 10:47:22 +0900

    백준1000

---
+++ 백준1000/소스.c
@@ -0,0 +1,11 @@
+# define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+int main(void) {
+	//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+	int A, B, result;
+	scanf("%d %d", &A, &B);
+	result = A + B;
+	printf("%d", result);
+
+	return 0;
+}
