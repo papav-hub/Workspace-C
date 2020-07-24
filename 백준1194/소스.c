5d17138e48177713a955b051cf79e9a120631ef3
혜민 최 <pavi03@kpu.ac.kr>
2020-07-24 11:10:17 +0900

    미완성

---
+++ 백준1194/소스.c
@@ -0,0 +1,24 @@
+# define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+int main() {
+	int nn;
+	scanf("%d", &nn);
+
+	int n = 2;
+	int a, b;
+	int count = 0;
+	/*while (1) {
+		for (int i = 1;; i++) {
+			for (int j = 1;; j++) {
+				a = i;
+				b = j;
+			}
+		}
+
+		if (nn == count) {
+			break;
+		}
+	}*/
+	printf("%d / %d", a, b);
+}
