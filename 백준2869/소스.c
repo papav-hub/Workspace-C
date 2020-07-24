ef39651baed35cdb9f2ac53ec7427ea75f4c58ac
혜민 최 <pavi03@kpu.ac.kr>
2020-07-24 09:46:49 +0900

    백준2869

--- 백준2869/소스.c
+++ 백준2869/소스.c
@@ -4,19 +4,13 @@
 int main() {
 	int A, B, V;
 	scanf("%d %d %d", &A, &B, &V);
-	int result = 0;
+	//V-B = 달팽이가 목표 지점에 도착한 날에는 미끌어지 않음으로 (A-B로 계산하려면)
 	int day = 0;
-	while (1) {
-		day++;
-		result += A;
-		if (result >= V) {
-			break;
-		}
-		else {
-			result -= B;
-		}
+	if ((V - A) % (A - B) == 0) {
+		day = (V - B) / (A - B);
+	}
+	else {//나누어 떨어지지 않으면 하루가 더 필요하므로 + 1
+		day = (V - B) / (A - B) + 1;
 	}
 	printf("%d", day);
-
-
 }
