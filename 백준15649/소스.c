c88a3e9026ea3640045b2ed142c901b2d8df2a09
혜민 최 <pavi03@kpu.ac.kr>
2020-07-30 14:14:34 +0900

    에베뷍라ㅓㅔ

--- 백준15649/소스.c
+++ 백준15649/소스.c
@@ -1,22 +1,58 @@
 # define _CRT_SECURE_NO_WARNINGS
 # include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	int N, M;
-	scanf("%d %d", &N, &M);
+int N, M;
+int array[1000];
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = i + 1; j <= N; j++) {
-			if (i != j) {
-				printf("%d %d", i, j);
+void print() {
+	for (int i = 0; i < M; i++) {
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+}
+
+int overlap() {//0이면 중복 없음 //1이면 중복
+	int check = 0;
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < M - 1; j++) {
+			if (array[i] == array[j]) {
+				check = 1;
 			}
 		}
+	}
+	return check;
+}
 
+void over() {//N을 넘어갔느냐
+	for (int i = 0; i < M; i++) {
+		if (array[i] > N) {
+			if (i == 0) {
+				exit(1);
+			}
+			else {
+				array[i - 1]++;
+				if (overlap() == 1) {
+					over();
 
+				}
+			}
+		}
+	}
+}
 
+int main() {
+	scanf("%d %d", &N, &M);
 
+	for (int i = 0; i < M; i++) {//처음 값
+		array[i] = i + 1;
+	}
+	print();
 
-		printf("\n");
+	while (1) {
+		array[N]++;
+		over();
+		print();
 	}
 
 
