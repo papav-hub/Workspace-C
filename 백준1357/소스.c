296a20fe0b7c38ef1d3800525b28444429818dbe
papav-hub <pavi03@kpu.ac.kr>
2020-08-18 15:01:59 +0900

    백준

---
+++ 백준1357/소스.c
@@ -0,0 +1,13 @@
+# define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+# include <stdlib.h>
+//ÇÏ´ÂÁß
+int main() {
+	int a[100] = { 0, }, b[100] = { 0, };
+	//int a[100], b[100];
+	scanf("%s %s", a, b);
+	int aa = atoi(a);
+	int bb = atoi(b);
+	int result = aa + bb;
+	printf("%d", result);
+}
